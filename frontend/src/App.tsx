b83db801280c48a58140ff903225fe81bdae3846
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2025-12-04 12:45:40 -0800

    feat: create navbar component structure and add to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +9,8 @@ function App() {
 
   return (
     <>
+      <Navbar/>
+      
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
