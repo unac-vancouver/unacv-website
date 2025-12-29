0bac9b6e1e39bb82fc995cbdb4df2236a32b747d
Elliott Schmechel <elliottschmechel@gmail.com>
2025-12-29 04:48:40 -0800

    feat: add FeaturedEvents component to display upcoming events with details and images

--- frontend/src/components/Events.tsx
+++ frontend/src/components/Events.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/EventsPageSections/Hero";
+import FeaturedEvents from "./EventsPageSections/FeaturedEvents";
 
 export default function EventsPage(){
     return (
         <main aria-labelledby="events-title">
             <Hero />
+            <FeaturedEvents />
         </main>
     );
 }
