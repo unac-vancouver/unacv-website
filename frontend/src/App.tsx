826061e05cbe79300ddcd2bd98892b9a5e7b1fcc
Axel Velasquez <yoloax11@gmail.com>
2026-01-04 18:38:24 -0600

    feat: add Contact route to enable navigation to the Contact page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import './App.css'
 import HomePage from '@/components/HomePage'
 import EventsPage from '@/components/Events'
 import GetInvolvedPage from '@/components/GetInvolved'
 import AboutPage from '@/components/About'
+import Contact from '@/components/Contact'
 
 function App() {
 
   return (
-    <div className="min-h-screen bg-blue-800 max-w-[1920px] mx-auto"> 
+    <div className="min-h-screen bg-blue-800 max-w-[1920px] mx-auto">
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/events" element={<EventsPage />} />
           <Route path="/get-involved" element={<GetInvolvedPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
