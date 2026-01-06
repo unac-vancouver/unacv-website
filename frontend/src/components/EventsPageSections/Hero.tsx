6065aff4c11a9a09e2f05f4324e7260aadedaede
Axel Velasquez <yoloax11@gmail.com>
2026-01-05 18:05:20 -0600

    fix: adjust padding in Hero component for improved layout

--- frontend/src/components/EventsPageSections/Hero.tsx
+++ frontend/src/components/EventsPageSections/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <section
             id="events-hero"
-            className="relative w-full flex flex-col items-center py-14 pb-14 px-6 md:px-20 lg:px-40"
+            className="relative w-full flex flex-col items-center py-20 px-6 md:px-20 lg:px-40"
         >
             {/* Background Image and Overlay */}
             <div aria-hidden="true" className="absolute inset-0 pointer-events-none overflow-hidden">
